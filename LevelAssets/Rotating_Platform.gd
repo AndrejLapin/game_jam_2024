7f98d09e68fd1b9277feeab2690924d9f3f41d00
Andrej Lapin <andrej.lapin@digital.ai>
2024-01-27 18:35:22 +0200

    little rotating platform fix

--- LevelAssets/Rotating_Platform.gd
+++ LevelAssets/Rotating_Platform.gd
@@ -5,13 +5,13 @@ var rng = RandomNumberGenerator.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	global_position.x = rng.randf_range(-5,5)
-	initial_position = self.get_global_position()
+	initial_position = position
 	global_rotation.z = rng.randf_range(0, 360)
 
 func _integrate_forces(state):
 	global_rotation.x = 0
 	global_rotation.y = 0
-	global_position = initial_position
+	position = initial_position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
