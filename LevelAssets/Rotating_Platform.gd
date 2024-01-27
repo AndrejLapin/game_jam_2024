c10e3fc3cf8198fab20c607eda5934b0a13d3146
lasive <109923383+lasive@users.noreply.github.com>
2024-01-27 17:20:50 +0200

    added rotating platform

---
+++ LevelAssets/Rotating_Platform.gd
@@ -0,0 +1,19 @@
+extends StaticBody3D
+var initial_position
+const ROTATION_SPEED = 0.5
+var rng = RandomNumberGenerator.new()
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	global_position.x = rng.randf_range(-5,5)
+	initial_position = self.get_global_position()
+	global_rotation.z = rng.randf_range(0, 360)
+
+func _integrate_forces(state):
+	global_rotation.x = 0
+	global_rotation.y = 0
+	global_position = initial_position
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	global_rotation.z += ROTATION_SPEED * delta
+	pass
