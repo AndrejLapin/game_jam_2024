8504095a5ca58b3d469c58031e0c319f5c607c4e
Andrej Lapin <andrej.lapin@digital.ai>
2024-01-27 21:52:16 +0200

    mouse input changes

--- input_collection.gd
+++ input_collection.gd
@@ -1,10 +1,15 @@
-extends Node
+extends Node3D
 
 var horizontal_direction: float = 0
 var vertical_direction: float = 0
+var mouse_just_pressed: bool = false
 var jump: bool = false
 var jump_held: bool = false
+var mouse_position: Vector2 = Vector2(0.0, 0.0)
+var rayOrigin: Vector3 = Vector3()
+var rayEnd: Vector3 = Vector3()
 
+@onready var camera: Camera3D = $Camera3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,10 +21,16 @@ func _process(delta):
 	pass
 
 
+func _input(event):
+	if event is InputEventMouseMotion:
+		mouse_position = event.position
+
+
 func _physics_process(delta):
 	# read input pass to child nodes
 	horizontal_direction = Input.get_axis("move_left", "move_right")
 	vertical_direction = Input.get_axis("move_up", "move_down")
+	mouse_just_pressed = Input.is_action_just_pressed("mouse_click")
 	if Input.is_action_just_pressed("jump"):
 		jump = true
 		jump_held = true
@@ -28,3 +39,12 @@ func _physics_process(delta):
 	
 	if Input.is_action_just_released("jump"):
 		jump_held = false
+	process_mouse_position()
+
+
+func process_mouse_position():
+	var space_state = get_world_3d().direct_space_state
+	
+	var mouse_position = get_viewport().get_mouse_position()
+	
+	#ray
