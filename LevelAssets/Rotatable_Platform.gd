8f4bd16318d76e23566a9eb05e6e550ee5c646e1
Andrej Lapin <andrej.lapin@digital.ai>
2024-01-27 18:32:10 +0200

    platform fixes

--- LevelAssets/Rotatable_Platform.gd
+++ LevelAssets/Rotatable_Platform.gd
@@ -4,12 +4,15 @@ const ROTATION_SPEED = 0.5
 var rng = RandomNumberGenerator.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	global_position.x = rng.randf_range(-5,5)
-	initial_position = self.get_global_position()
+	position.x = rng.randf_range(-5,5)
+	initial_position = position
 
 func _integrate_forces(state):
 	global_rotation.x = 0
 	global_rotation.y = 0
+	linear_velocity.y = 0
+	#linear_velocity.x = 0
+	linear_velocity.z = 0
 	position = initial_position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
